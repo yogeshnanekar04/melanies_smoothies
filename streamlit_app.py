489800b32ba5abb3e060ad3f2e1382f39fc24697
yogeshnanekar04 <67158277+yogeshnanekar04@users.noreply.github.com>
2025-10-04 17:19:56 -0500

    Update streamlit_app.py
    Put the JSON into a Dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,4 +48,6 @@ if ingredients_list:
         st.success(f'Your Smoothie is ordered, {name_on_order}!', icon="✅")
 
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response.json())
+# st.text(smoothiefroot_response.json())
+st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+
