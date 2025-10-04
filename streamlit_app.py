58a3e16760d0e4db8b23844f49fa2aeac3ffde70
yogeshnanekar04 <67158277+yogeshnanekar04@users.noreply.github.com>
2025-10-04 17:18:07 -0500

    Update streamlit_app.py
    show json string  response

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,4 +48,4 @@ if ingredients_list:
         st.success(f'Your Smoothie is ordered, {name_on_order}!', icon="✅")
 
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
+st.text(smoothiefroot_response.json())
